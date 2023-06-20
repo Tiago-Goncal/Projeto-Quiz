59b8ec347479acc82eec0c5695f7f03f8b5663de
Tiago Gonçalves <137051434+Tiago-Goncal@users.noreply.github.com>
2023-06-20 12:13:31 +0100

    Estrutura menu
    yep

--- main.cpp
+++ main.cpp
@@ -22,3 +22,54 @@ int main()
 
     return 0;
 }
+
+int menu()
+{
+    int escolha = 0;
+
+    while(escolha != 5){
+    if (int login = 0){
+
+    switch (escolha){
+    case 1:
+        //Iniciar jogo
+        break;
+    case 2:
+        //Tabela de lideres
+        break;
+    case 3:
+        //Eliminar utilizador
+        break;
+    case 4:
+        //Sair
+        break;
+    default:
+        //BRR! Erro
+        break;}
+
+    }else{
+
+    switch (escolha){
+
+    case 1:
+        //Iniciar sessão
+        break;
+    case 2:
+        //Registar
+        break;
+    case 3:
+        //Iniciar jogo
+        break;
+    case 4:
+        //Tabela de lideres
+        break;
+    case 5:
+        //Sair
+        break;
+    default:
+        //BRR! Erro
+        break;
+    }
+    }
+}
+}

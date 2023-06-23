e4af71b1051cc3ac1fff01ebc087113b6a889926
Tiago Gonçalves <137051434+Tiago-Goncal@users.noreply.github.com>
2023-06-23 17:36:30 +0100

    tema
    we

---
+++ tema.hpp
@@ -0,0 +1,45 @@
+#ifndef TEMA_H
+#define TEMA_H
+
+#include <iostream>
+
+using namespace std;
+
+
+void tema()
+{
+    setlocale(LC_ALL,"");
+    system("CLS");
+    int tema = 0;
+    
+    cout << "\nEscolha o tema:" << endl;
+    cout << "[1]:Cultura Geral" << endl;
+    cout << "[2]:Historia" << endl;
+    cout << "[3]:Desporto" << endl;
+    cout << "[4]:Cinema" << endl;
+    cout << ">>>>" << endl;
+    cin >> tema;
+
+    switch(tema){
+        case 1:
+        //Cultura Geral
+            break;
+        case 2:
+        //Historia
+            break;
+        case 3:
+        //Desporto
+            break;
+        case 4:
+        //Cinema
+            break;
+        default:
+        //Erro!
+            break;
+
+
+    }
+
+
+
+}

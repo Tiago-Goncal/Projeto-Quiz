723358221d577ebea97a827f7e70831b7e880d75
Tiago Gonçalves <137051434+Tiago-Goncal@users.noreply.github.com>
2023-06-26 09:53:01 +0100

    b

--- Menuprime.hpp
+++ Menuprime.hpp
@@ -30,41 +30,6 @@ void menu()
 
     while(escolha != 4)
     {
-   /* if (loginstatus == 0){
-
-    cout << "\nSelecione:" << endl;
-    cout << "[1]:Iniciar jogo" << endl;
-    cout << "[2]:Tabela de lideres" << endl;
-    cout << "[3]:Eliminar utilizador" << endl;
-    cout << "[4]:Sair" << endl;
-    cout << ">>>>" << endl;
-    cin >> escolha;
-    switch (escolha){
-    case 1:
-        system("CLS");
-        //Iniciar jogo
-        break;
-    case 2:
-        system("CLS");
-        //Tabela de lideres
-        break;
-    case 3:
-        system("CLS");
-        //Eliminar utilizador
-        break;
-    case 4:
-        system("CLS");
-        //vexit();
-        break;
-    default:
-        //error();
-        //BRR! Erro
-        break;
-    }
-
-    }
-    else if(loginstatus == 1)
-    {*/
     cout << "\nSelecione:" << endl;
     cout << "[1]:Iniciar sessão" << endl;
     cout << "[2]:Registar" << endl;
@@ -100,9 +65,6 @@ void menu()
         //BRR! Erro
         break;
     }
-    /*}else{
-        return;
-    }*/
 }
 
 
